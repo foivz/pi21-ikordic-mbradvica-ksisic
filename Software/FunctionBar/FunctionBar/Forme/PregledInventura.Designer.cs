021369fca70373ed4cd38dbb1bf5e78523a95b3b
Klara <ksisic@foi.hr>
2021-09-12 12:57:44 +0200

    Dodavanje pregleda svih inventura

---
+++ Software/FunctionBar/FunctionBar/Forme/PregledInventura.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace FunctionBar.Forme
+{
+    partial class PregledInventura
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvInventure = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvStavke = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStavke)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvInventure
+            // 
+            this.dgvInventure.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventure.Location = new System.Drawing.Point(66, 72);
+            this.dgvInventure.Name = "dgvInventure";
+            this.dgvInventure.RowHeadersWidth = 51;
+            this.dgvInventure.RowTemplate.Height = 24;
+            this.dgvInventure.Size = new System.Drawing.Size(536, 457);
+            this.dgvInventure.TabIndex = 0;
+            this.dgvInventure.SelectionChanged += new System.EventHandler(this.PrikaziArtikle);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Provedene inventure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(748, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stavke";
+            // 
+            // dgvStavke
+            // 
+            this.dgvStavke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStavke.Location = new System.Drawing.Point(754, 72);
+            this.dgvStavke.Name = "dgvStavke";
+            this.dgvStavke.RowHeadersWidth = 51;
+            this.dgvStavke.RowTemplate.Height = 24;
+            this.dgvStavke.Size = new System.Drawing.Size(536, 457);
+            this.dgvStavke.TabIndex = 4;
+            // 
+            // PregledInventura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1395, 656);
+            this.Controls.Add(this.dgvStavke);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvInventure);
+            this.Name = "PregledInventura";
+            this.Text = "PregledInventura";
+            this.Load += new System.EventHandler(this.PregledInventura_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStavke)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvInventure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvStavke;
+    }
+}
---
+++ Software/FunctionBar/FunctionBar/Forme/PregledInventura.cs
@@ -0,0 +1,88 @@
+﻿using FunctionBarDb;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunctionBar.Forme
+{
+    public partial class PregledInventura : Form
+    {
+        public PregledInventura()
+        {
+            InitializeComponent();
+        }
+
+        //dodavanje vrijednosti provedenih inventura u datagridview pozivajuci metodu DohvatiInventure
+        private void PregledInventura_Load(object sender, EventArgs e)
+        {
+            dgvInventure.DataSource = null;
+            dgvInventure.DataSource = DohvatiInventure();
+        }
+
+        //dohvaćanje provedenih inventura iz baze podataka tre vracanje liste provedenih inventura
+        private object DohvatiInventure()
+        {
+            using (var context = new FunctionBarDB())
+            {
+                var upit = from inventura in context.inventuras
+                           join zaposlenik in context.zaposleniks on inventura.OIB equals zaposlenik.OIB
+                           select new
+                           {
+                               Id_inventure = inventura.ID,
+                               Datum_inventure = inventura.datum_inventure,
+                               Ime_prezime = zaposlenik.ime + " " + zaposlenik.prezime
+                           };
+
+                return upit.ToList();
+            }
+        }
+
+        //odabirom odredene inventure, poziva se metoda DohvatiArtikleInventure
+        /*private void PrikaziArtikle(object sender, EventArgs e)
+        {
+            var index = dgvInventure.CurrentRow.Cells[0].Value;
+            DohvatiArtikleInventure((int)index);
+        }*/
+
+
+
+        //Dohvacanje artikala, tocnije stavaka provedene inventure iz baze podataka
+        private void DohvatiArtikleInventure(int id)
+        {
+            using (var context = new FunctionBarDB())
+            {
+                var upit = from artikli_inventure in context.artikli_inventure
+                           join artikl in context.artikls on artikli_inventure.ID_artikla equals
+                           artikl.ID
+                           where artikli_inventure.ID_inventure == id
+                           select new
+                           {
+                               Artikl = artikl.naziv,
+                               Količina_na_zalihi = artikli_inventure.kolicina_na_zalihi,
+                               Višak_manjak = artikli_inventure.visak_manjak,
+                           };
+                
+                dgvStavke.DataSource = upit.ToList();
+            }
+        }
+
+        private void PrikaziArtikle(object sender, EventArgs e)
+        {
+            var index = dgvInventure.CurrentRow.Cells[0].Value;
+            DohvatiArtikleInventure((int)index);
+        }
+
+        private void PregledInventura_HelpRequested(object sender, HelpEventArgs hlpevent)
+        {
+            /*string helpPoveznica = "FunctionBarHelp.chm";
+            Help.ShowHelp(this, Environment.CurrentDirectory + "\\Resources\\" + helpPoveznica, HelpNavigator.Topic, "start.htm#_Toc43663407");*/
+        }
+
+    }
+}
