2076760cf58c6a4152d66855f9616a8700d023d7
Klara <ksisic@foi.hr>
2021-09-12 13:42:08 +0200

    Popravak pregleda

--- Software/FunctionBar/FunctionBarDb/Model1.Designer.cs
+++ Software/FunctionBar/FunctionBarDb/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'B:\pi\pi21-ikordic-mbradvica-ksisic\Software\FunctionBar\FunctionBarDb\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Klara\Desktop\PI_vjezbanje\pi21-ikordic-mbradvica-ksisic\Software\FunctionBar\FunctionBarDb\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
